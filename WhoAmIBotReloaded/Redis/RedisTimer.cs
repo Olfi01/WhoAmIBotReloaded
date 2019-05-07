01249066a507748a7af7e62d23de01d9eefe384b
Florian Meyer <florian.meyer@capgemini.com>
2019-05-07 12:11:41 +0200

    Maybe giving the serializer the rights to modify the class could help

--- WhoAmIBotReloaded/Redis/RedisTimer.cs
+++ WhoAmIBotReloaded/Redis/RedisTimer.cs
@@ -8,11 +8,11 @@ namespace WhoAmIBotReloaded.Redis
 {
     public class RedisTimer
     {
-        public TimerType Type { get; }
+        public TimerType Type { get; set; }
         public DateTimeOffset TimerEnd { get; set; }
-        public string GameId { get; }
-        public long ChatId { get; }
-        public string TimerId { get; }
+        public string GameId { get; set; }
+        public long ChatId { get; set; }
+        public string TimerId { get; set; }
         public int ExtraValue { get; set; }
 
         public RedisTimer(TimerType type, DateTimeOffset timerEnd, string gameId, long chatId, int extraValue = 0)
